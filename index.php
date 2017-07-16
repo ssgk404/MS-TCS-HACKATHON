926b70e6dcd2a649a144a66b3a6a7ed81881dd87
ssgk404 <surendra.2@tcs.com>
2017-07-16 12:52:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <title>Surendra Singh Gurjar</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
   <link rel="icon" href="favicon.ico" type="image/x-icon">
